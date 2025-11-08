302859092aebe2f1d4486d68714f44dc9680851b
asifalam515 <asibulalam515@gmail.com>
2025-11-08 21:45:51 +0600

    tuple

---
+++ basic_typeScript/src/nonPrimitives.ts
@@ -0,0 +1,9 @@
+// array and Object
+// ts tuple
+let bazarList: string[] = ["eggs", "rice", "banana", "sugar", "milk"];
+let mixedArray: (string | number)[] = ["eggs", 12, "milk", 1, "sugar", 5];
+mixedArray.push("234");
+mixedArray.push("apple");
+
+let coordinates: [number, number] = [20, 30];
+let couple: [string, string] = ["asif", "eva"];
---
+++ basic_typeScript/src/primitive.ts
@@ -0,0 +1,7 @@
+//  JS-> TS
+// string ,number ,boolean ,undefined,null
+
+//TS:never ,unknown,void
+
+let userName: string = "asibul alam";
+let num = 123;

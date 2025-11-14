2fd84dc59da1c40ad311bd3824f51e36d6063be7
asifalam515 <asibulalam515@gmail.com>
2025-11-14 23:02:35 +0600

    key of

--- basic_typeScript/src/keyOf.ts
+++ basic_typeScript/src/keyOf.ts
@@ -1,10 +1,23 @@
-// key of operator =>type operator
+{
+  // key of operator =>type operator
 
-type RichPeoplesVehicle = {
-  car: string; //key :value
-  bike: string;
-  cng: string;
-};
-type MyVehicle1 = "bike" | "car" | "cng";
-type MyVehicle2 = keyof RichPeoplesVehicle;
-const myV: MyVehicle1 = "car";
+  type RichPeoplesVehicle = {
+    car: string; //key :value
+    bike: string;
+    cng: string;
+  };
+  type MyVehicle1 = "bike" | "car" | "cng";
+  type MyVehicle2 = keyof RichPeoplesVehicle;
+  const myV: MyVehicle1 = "car";
+
+  const user = {
+    id: 222,
+    name: "asif",
+    address: {
+      city: "ctg",
+    },
+  };
+  const myId = user["id"];
+  const myName = user["name"];
+  const myAddress = user["address"];
+}

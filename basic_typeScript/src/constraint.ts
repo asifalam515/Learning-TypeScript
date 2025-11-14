15a19b79e3e77c4f618c58ea1d33ed6699822a27
asifalam515 <asibulalam515@gmail.com>
2025-11-14 22:49:01 +0600

    constrant applied

--- basic_typeScript/src/constraint.ts
+++ basic_typeScript/src/constraint.ts
@@ -1,1 +1,28 @@
-// contrain:strict rules deya
+{
+  // contraint:strict kono rules deya
+  type TStudent = { id: number; name: string };
+  const addStudentToCourse = <T extends TStudent>(studentInfo: T) => {
+    return {
+      course: "Next Level Web Course",
+      ...studentInfo,
+    };
+  };
+  const student1 = {
+    id: 1234,
+    name: "asif",
+    hasPen: true,
+  };
+  const student2 = {
+    id: 1232,
+    name: "Jhankar vai",
+    hasCar: true,
+    isMarried: true,
+  };
+  const student3 = {
+    id: 123,
+    name: "sakib",
+    hasWatch: true,
+  };
+  const result = addStudentToCourse(student3);
+  console.log(result);
+}
--- basic_typeScript/src/genericFunction.ts
+++ basic_typeScript/src/genericFunction.ts
@@ -9,8 +9,39 @@
     return [{ value }];
   };
 
-  const arrString = createArrayWithString("hello bruder");
-  const numberArray = createArrayWithNumber(22);
-  const arrObj = createArrayWithUserObj({ id: 123, name: "asibul alam" });
+  const createArrayWithGeneric = <T>(value: T) => [value];
+
+  const arrString = createArrayWithGeneric("string");
+  const arrNumber = createArrayWithGeneric(234);
+  const arrObject = createArrayWithGeneric({ id: 232, name: "asif" });
+
+  // tuple with generic
+  const createTupleWithGeneric = <X, Y>(value1: X, value2: Y) => {
+    return [value1, value2];
+  };
+  const res1 = createTupleWithGeneric(1, 2);
+  const res2 = createTupleWithGeneric("asi re vai achi", {
+    name: "iphone",
+    price: 1233,
+  });
+
+  // const addStudent to course
+  const addStudentToCourse = <T>(studentInfo: T) => {
+    return { course: "Next Level developement", ...studentInfo };
+  };
+  const student1 = {
+    id: 1234,
+    name: "asif",
+    hasPen: true,
+  };
+  const student2 = {
+    id: 1232,
+    name: "Jhankar vai",
+    hasCar: true,
+    isMarried: true,
+  };
+  const result = addStudentToCourse(student1);
+  const result2 = addStudentToCourse(student2);
+  console.log(result2);
   // ..........
 }

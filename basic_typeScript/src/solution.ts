5ffd099bf9055729fd313f37ab433e0b7fdae522
asifalam515 <asibulalam515@gmail.com>
2025-11-16 22:41:22 +0600

    added

--- basic_typeScript/src/solution.ts
+++ basic_typeScript/src/solution.ts
@@ -1,10 +1,14 @@
-const formatValue = (value: string | number | boolean) => {
+const formatValue = (
+  value: string | number | boolean
+): string | number | boolean | undefined => {
   if (typeof value === "string") {
     return value.toUpperCase();
   } else if (typeof value === "number") {
     return value * 10;
   } else if (typeof value === "boolean") {
     return !value;
+  } else {
+    return undefined;
   }
 };
 // Create a function getLength that accepts a value which may be a string or an array, and returns the length of the value.
@@ -14,3 +18,11 @@ const formatValue = (value: string | number | boolean) => {
 // Requirements:
 // You must write the correct type for the function parameter and the return type.
 // You must use type checking to handle each case (typeof or Array.isArray).
+const getLength = (value: string | string[]) => {
+  let length = 0;
+  for (let i = 0; i < value.length; i++) {
+    length++;
+  }
+  return length;
+};
+console.log(getLength([10, 20, 30, 40]));
